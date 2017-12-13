5c963ae33a6ecfe7f0559dcbbf6573347f5361ba
Mathieu GONON <mgonon@student.42.fr>
2017-12-13 15:23:14 +0100

    Gallery OK

--- gallery.php
+++ gallery.php
@@ -1,1 +1,35 @@
 <?php require_once('Views/header.php') ?>
+
+<div class="gallery">
+    <div class="wrapper">
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+        <div><img src="assets/pics/test.png" alt=""></div>
+    </div>
+</div>
+<?php require_once('Views/footer.php') ?>
