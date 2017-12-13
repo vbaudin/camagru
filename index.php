b6bbef23c5088532484da8b23d75fe668cf035b9
Mathieu GONON <mgonon@student.42.fr>
2017-12-13 19:04:18 +0100

    frame selection

--- index.php
+++ index.php
@@ -2,13 +2,29 @@
 <div class="fake-row"></div>
 <div class="row flex-wrap">
     <aside class="take-pics">
+      <div class="toto">
         <h3>Take your pics</h3>
         <div class="flex-center">
           <video class="video" id="video"></video>
           <button class="btn-take-pic" id="startbutton">Prendre une photo</button>
         </div>
-        <!-- <canvas id="canvas"></canvas> -->
-        <!-- <img src="http://placekitten.com/g/320/261" id="photo" alt="photo"> -->
+      </div>
+      <div class="frame flex-center">
+        <div class="flex-center">
+          <img src="assets/pics/frames/boat.png" alt="boat frame">
+          <input type="checkbox" id="frame-boat">
+        </div>
+        <div class="flex-center">
+          <img src="assets/pics/frames/flower.png" alt="flower frame">
+          <input type="checkbox" id="frame-flower">
+        </div>
+        <div class="flex-center">
+          <img src="assets/pics/frames/river.png" alt="river frame">
+          <input type="checkbox" id="frame-river">
+        </div>
+      </div>
+      <!-- <canvas id="canvas"></canvas> -->
+      <!-- <img src="http://placekitten.com/g/320/261" id="photo" alt="photo"> -->
     </aside>
     <aside class="last-pics">
         <h3>Last pics</h3>
@@ -19,7 +35,7 @@
     </aside>
 </div>
 
-<div class="fake-row"></div>
+<div class="fake-row" id="test"></div>
 <div class="fake-row"></div>
 <div class="fake-row"></div>
 <div class="fake-row"></div>
@@ -31,18 +47,30 @@
   var streaming = false,
       video        = document.querySelector('#video'),
       cover        = document.querySelector('#cover'),
-      // canvas       = document.querySelector('#canvas'),
       photo        = document.querySelector('#photo'),
       startbutton  = document.querySelector('#startbutton'),
       lastpics     = document.querySelector('#lastpics'),
+      frames       = Array.from(document.querySelectorAll('*[id^="frame"]'))
       width = 320,
       height = 0
-
   navigator.getMedia = ( navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
                          navigator.mozGetUserMedia ||
                          navigator.msGetUserMedia)
 
+  const frameHandler = (e) => {
+    // console.log(e)
+    const frames = Array.from(document.querySelectorAll('*[id^="frame"]'))
+    if (e.srcElement.checked === true){
+      frames.forEach(frame => frame.checked = false)
+      e.srcElement.checked = true
+    }
+    else{
+      e.srcElement.checked = false
+    }
+  }
+  frames.forEach(frame => frame.addEventListener('change', frameHandler));
+// console.log(frames)
   navigator.getMedia(
     {
       video: true,
@@ -75,13 +103,20 @@
 
   function takepicture() {
 
-    const canvas        = document.createElement("canvas");
+    const canvas          = document.createElement("canvas")
+    const canvas_buttons  = document.createElement("div")
+    // const test            = document.querySelector('#test')
+    // canvas_buttons.innerHTML = "sample text"
     // const canvasContent = document.createTextNode("Salutations !")
     canvas.width = width
     canvas.height = height
     canvas.getContext('2d').drawImage(video, 0, 0, width, height)
     const data = canvas.toDataURL('image/png')
+    // canvas_buttons.style.width = "20px"
+    // canvas_buttons.style.height = "20px"
+    // canvas_buttons.style.backgroundColor = "blue"
     lastpics.appendChild(canvas)
+    // test.appendChild(canvas_buttons)
 
     // const xhr = new XMLHttpRequest()
     // xhr.open("POST", 'index.php', true)
